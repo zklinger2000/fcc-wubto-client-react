84d573a60ca26f886ac7e4b7e697d0563fd15c91
Zachary Klinger <zklinger@zmac.local>
2017-07-13 09:10:35 -0700

    Homepage(style): Design a new home page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,6 +2,7 @@ import React, {
   Component,
   PropTypes
 } from 'react';
+import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import './HomePage.scss';
@@ -9,24 +10,35 @@ import ScrollToTopOnMount from '../ScrollToTopOnMount/ScrollToTopOnMount';
 
 class HomePage extends Component {
   render() {
+    const { current } = this.props;
     return (
       <section className="home-page">
         <ScrollToTopOnMount/>
         <Helmet
-          title="FCC Facebook Client React"
-          titleTemplate="%s | A Full Stack React application with Facebook Login"
+          title="Wubto"
+          titleTemplate="%s | Choose a new hangout. Let your friends know you'll be there."
         />
         <header>
-          <h1>FCC Facebook Client React</h1>
-          <p>A Full Stack React application with Facebook Login</p>
+          <h1>Wubto</h1>
+          <h2>Choose a new hangout.<br/> Let your friends know you'll be there.</h2>
         </header>
+        <section className="location">
+          <p>Current Location: {current.display_address && current.display_address[1] || ''}</p>
+        </section>
+
+        <section>
+          <Link to="/search"><button className="btn btn-primary">Find</button></Link> a new place
+        </section>
+
+        {!this.props.authenticated && (
+          <section>
+            <a href={`${process.env.NODE_ENV === 'production' ? 'https://fcc-heroku-wubto-rest-api.herokuapp.com' : 'http://localhost:8050'}/login/facebook`} target="_self"><button className="btn btn-primary">Login</button></a> to see where your friends will be
+          </section>
+        )}
+
         {this.props.authenticated && this.props.user && this.props.user.displayName && (
-          <section className="container">
-            <p className="jumbotron">
-              Hello there, {this.props.user.displayName}.
-              You have been authenticated with Facebook.<br/>
-              There is now a link to the page "Private" in the Header and Footer navigation.
-            </p>
+          <section>
+            See where your <Link to="/friends"><button className="btn btn-primary">friends</button></Link> will be
           </section>
         )}
      </section>
@@ -36,13 +48,17 @@ class HomePage extends Component {
 
 HomePage.propTypes = {
   user: PropTypes.object.isRequired,
-  authenticated: PropTypes.bool.isRequired
+  authenticated: PropTypes.bool.isRequired,
+  search: PropTypes.object.isRequired,
+  current: PropTypes.object.isRequired,
 };
 
 function mapStateToProps(state) {
   return {
     user: state.auth.user,
-    authenticated: state.auth.authenticated
+    authenticated: state.auth.authenticated,
+    search: state.yelp.search,
+    current: state.yelp.current
   };
 }
 
