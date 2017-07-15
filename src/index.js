7fe3366b54fd86503894b3cc30eb41d13c28127e
Zachary Klinger <zklinger@zmac.local>
2017-07-14 21:23:23 -0700

    rest(fix): Put in the correct name for the REST API url

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const history = syncHistoryWithStore(browserHistory, store);
     token based auth system.
  */
 const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://fcc-heroku-rest-api.herokuapp.com'
+  ? 'https://fcc-wubto-rest-api.herokuapp.com'
   : 'http://localhost:8050';
 
 const userToken = localStorage.getItem('user_token');
