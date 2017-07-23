436ddfe1f50eea1c1d449156de356875b32ddaf7
Zachary Klinger <zklinger@zmac.local>
2017-07-22 18:19:28 -0700

    Yelp(map): Create Google Embed Map component

---
+++ src/components/EmbedMap/EmbedMap.js
@@ -0,0 +1,61 @@
+import React, {
+  Component,
+  PropTypes,
+} from 'react';
+
+class EmbedMap extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      active: false,
+      timerID: undefined
+    };
+
+    this.toggleClass = this.toggleClass.bind(this);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.state.timerID);
+  }
+
+  toggleClass() {
+    console.log('map clicked');
+    clearTimeout(this.state.timerID);
+    this.setState({ active: true });
+    this.state.timerID = setTimeout(
+      () => this.setState({ active: false }),
+      6000
+    );
+  }
+
+  render() {
+    const { place } = this.props;
+
+    return (
+      <section className="google-map" >
+        <div className={this.state.active ? "overlay active" : "overlay"} onClick={this.toggleClass}/>
+        <iframe
+          width="320"
+          height="260"
+          frameBorder="0" style={{border:0}}
+          src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyA2y91bDTIXUkBxXfHWkQwuHZuGqz4ClY4
+              &q=${place.location && place.location.display_address && place.location.display_address.join(',')}`}
+          allowFullScreen
+        />
+        <div className="underlay">
+          <i className="fa fa-cog fa-spin"/>
+        </div>
+      </section>
+    );
+  }
+}
+
+EmbedMap.propTypes = {
+  place: PropTypes.object.isRequired
+};
+EmbedMap.defaultProps = {
+  place: {}
+};
+
+export default EmbedMap;
